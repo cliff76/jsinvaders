5212f9995a0099275def52f4c12c689238a7ea28
Clifton Craig <clifton.craig@gmail.com>
2020-06-13 13:15:47 -0700

    Add initial game area

---
+++ main.js
@@ -0,0 +1,32 @@
+var gamePiece;
+
+function startGame() {
+    gameArea.start();
+    gamePiece = new component(30, 30, "red", 10, 120);
+  }
+  
+  var gameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+      this.canvas.width = 1280;
+      this.canvas.height = 800;
+      this.canvas.style = "border:1px solid #000000;";
+      this.context = this.canvas.getContext("2d");
+      document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+  }
+
+  function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    ctx = gameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  startGame();
