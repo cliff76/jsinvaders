3f72aa247e0b7323357567e2425a0a33a48a39af
Clifton Craig <clifton.craig@gmail.com>
2020-06-13 13:24:29 -0700

    Add update interval to game start

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ function startGame() {
       this.canvas.style = "border:1px solid #000000;";
       this.context = this.canvas.getContext("2d");
       document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+      this.interval = setInterval(updateGameArea, 20);
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -37,4 +38,3 @@ function startGame() {
   }
 
   startGame();
-  updateGameArea();
