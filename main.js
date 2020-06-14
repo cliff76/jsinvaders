8fd40c6c0ec12dcf77da8c841ddbad0500024245
Clifton Craig <clifton.craig@gmail.com>
2020-06-14 15:37:02 -0700

    Clean up code, formatting

--- main.js
+++ main.js
@@ -3,18 +3,18 @@ const configuration = {
 }
 
 var gamePieces;
-  
+
 var gameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-      this.canvas.width = 1280;
-      this.canvas.height = 800;
-      this.canvas.style = "border:1px solid #000000;";
-      this.context = this.canvas.getContext("2d");
-      document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-      this.interval = setInterval(gameLoop, 20);
+    canvas: document.createElement("canvas"),
+    start: function () {
+        this.canvas.width = 1280;
+        this.canvas.height = 800;
+        this.canvas.style = "border:1px solid #000000;";
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(gameLoop, 20);
     },
-    clear : function() {
+    clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     },
     keysPressed: {
@@ -22,25 +22,25 @@ var gameArea = {
         right: false,
         shoot: false
     }
-  }
+}
 
 function buildGamePieces() {
     const ship = buildShip();
     const fallingBlock = new component(30, 30, "red", 10, 120);
-    fallingBlock.move = function(){
+    fallingBlock.move = function () {
         this.y += 1;
     }
     return [ship, fallingBlock];
 }
 
 function buildShip() {
-    const sizeOfShip = {height: 30, width: 50};
+    const sizeOfShip = { height: 30, width: 50 };
     const gapFromBottom = 10;
     const nearBottomOfScreen = gameArea.canvas.height - (sizeOfShip.height + gapFromBottom);
-    const shipMiddleOfScreen = (gameArea.canvas.width/2) - (sizeOfShip.width/2);
-    const ship =  new component(sizeOfShip.width, sizeOfShip.height, "yellow", shipMiddleOfScreen, nearBottomOfScreen);
+    const shipMiddleOfScreen = (gameArea.canvas.width / 2) - (sizeOfShip.width / 2);
+    const ship = new component(sizeOfShip.width, sizeOfShip.height, "yellow", shipMiddleOfScreen, nearBottomOfScreen);
     ship.direction = 0;
-    ship.move = function(){
+    ship.move = function () {
         ship.x += ship.direction;
     }
     return ship;
@@ -50,43 +50,43 @@ function startGame() {
     addDocumentEventListeners();
     gameArea.start();
     gamePieces = buildGamePieces();
-  }
+}
 
-  function component(width, height, color, x, y) {
+function component(width, height, color, x, y) {
     this.width = width;
     this.height = height;
     this.x = x;
     this.y = y;
-    this.update = function(){
+    this.update = function () {
         ctx = gameArea.context;
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-    this.move = function(){
+    this.move = function () {
         this.x += 1;
     }
-  }
+}
 
-  /*
-  Document event listener listens to the HTML document for keyboard input
-  */
-  function addDocumentEventListeners() {
-      document.addEventListener('keydown', event => {
-        if(event.keyCode == 37) {
+/*
+Document event listener listens to the HTML document for keyboard input
+*/
+function addDocumentEventListeners() {
+    document.addEventListener('keydown', event => {
+        if (event.keyCode == 37) {
             gameArea.keysPressed.left = true;
-        } else if(event.keyCode == 39) {
+        } else if (event.keyCode == 39) {
             gameArea.keysPressed.right = true;
         }
-      });
+    });
 
-      document.addEventListener('keyup', event => {
-        if(event.keyCode == 37) {
+    document.addEventListener('keyup', event => {
+        if (event.keyCode == 37) {
             gameArea.keysPressed.left = false;
-        } else if(event.keyCode == 39) {
+        } else if (event.keyCode == 39) {
             gameArea.keysPressed.right = false;;
         }
-      });
-    }
+    });
+}
 
 function handleInput(keysPressed) {
     const ship = gamePieces[0];
@@ -98,10 +98,10 @@ function handleInput(keysPressed) {
     }
 }
 
-  function gameLoop() {
+function gameLoop() {
     gameArea.clear();
     handleInput(gameArea.keysPressed);
-    gamePieces.forEach( eachPiece => { eachPiece.move(); eachPiece.update();})
-  }
+    gamePieces.forEach(eachPiece => { eachPiece.move(); eachPiece.update(); })
+}
 
-  startGame();
+startGame();
